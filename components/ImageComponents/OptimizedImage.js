160dfc56592e3db16d0f8a8dfeb81e2eae875941
Rocky Li <rocky.li@scmp.com>
2021-01-08 18:15:58 +0800

    #i17-add-responsive-to-optimized-image

--- components/ImageComponents/OptimizedImage.js
+++ components/ImageComponents/OptimizedImage.js
@@ -1,12 +1,15 @@
+import Lazyload from 'react-lazyload'
 import React from 'react'
 
 const OptimizedImage = ({ filename, ...props }) => (
-  <img 
-    className={'width-height--100p object-fit--cover'}
-    src={require(`../../public/img/${filename}.jpg?resize&size=1920&format=webp`)}
-    alt={filename}
-    {...props}
-  />
+  <Lazyload>
+    <img 
+      className={'width-height--100p object-fit--cover'}
+      src={require(`../../public/img/${filename}.jpg?resize&sizes[]=300&sizes[]=600&sizes[]=1000&format=webp`)}
+      alt={filename}
+      {...props}
+    />
+  </Lazyload>
 )
 
 export default OptimizedImage
