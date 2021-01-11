6c8f91be4692cfa84d9e4788680b02f54b5f5e7e
Rocky Li <rocky.li@scmp.com>
2021-01-11 17:30:46 +0800

    #i25-update-to-align-optimized-responsive-breakpoints-with-next-image

--- components/ImageComponents/OptimizedImage.js
+++ components/ImageComponents/OptimizedImage.js
@@ -3,7 +3,7 @@ import React from 'react'
 const OptimizedImage = ({ filename, ...props }) => (
   <img 
     className={'width-height--100p object-fit--cover'}
-    src={require(`../../public/img/${filename}.jpg?resize&sizes[]=576&sizes[]=768&sizes[]=992&sizes[]=1200&format=webp`)}
+    src={require(`../../public/img/${filename}.jpg?resize&sizes[]=640&sizes[]=740&sizes[]=828&sizes[]=1080&sizes[]=1920&sizes[]=2048&sizes[]=3840&format=webp`)}
     alt={filename}
     {...props}
   />
